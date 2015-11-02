0490d1a17954490cdbe25c4b0c2ac14786b45b00
Christian Tegnér <christian.tegner@gmail.com>
2015-11-02 14:15:07 +0100

    Add Babel 6 plugins/presets

--- bin/create-bundle.js
+++ bin/create-bundle.js
@@ -3,6 +3,12 @@ var babelify = require('babelify');
 
 module.exports = function () {
     return browserify({ debug: true })
-        .transform(babelify)
+        .transform(babelify, {
+            presets: ['es2015', 'stage-0'],
+            plugins: [
+                'syntax-jsx',
+                'transform-react-jsx'
+            ]
+        })
         .require(require.resolve('../index.jsx'), { entry: true });
 };
